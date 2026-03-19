0d89de970069d1365943a87b12d9d89f51ef1150
udindindun <105261000+dndxray@users.noreply.github.com>
2026-03-19 20:05:40 +0700

    Add files via upload
    materi translasi dan rotasi dengan menggunakan opengl, serta membuat ilustrasi 2d mobil

---
+++ Pertemuan02/rotasi/02-rotasi.cpp
@@ -0,0 +1,27 @@
+#include <gl/glut.h>
+
+void Segitiga(void)
+{
+//     Membersihkan layar
+    glClear(GL_COLOR_BUFFER_BIT);
+    glTranslatef(0.25, -0.25, 0);
+    glRotated(60.0, 0.0, 0.0, 1.0);
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f); glVertex3f(-0.05, -0.05, 0.00); // Merah
+        glColor3f(0.0f, 1.0f, 0.0f); glVertex3f(0.15, -0.05, 0.00);  // Hijau
+        glColor3f(0.0f, 0.0f, 1.0f); glVertex3f(-0.05, 0.05, 0.00);  // Biru
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(640, 480);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA); 
+    glutCreateWindow("Segitiga");
+    glutDisplayFunc(Segitiga);
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f); 
+    glutMainLoop();
+    return 0;
+}
